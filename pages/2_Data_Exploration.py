e17cc5d3edbe0997e9af8aeb89fc347b76e4659d
Austen Suqi <97565129+Austen-Suqi@users.noreply.github.com>
2026-02-18 11:38:27 -0500

    Update 2_Data_Exploration.py

--- pages/2_Data_Exploration.py
+++ pages/2_Data_Exploration.py
@@ -1,14 +1,8 @@
-# Importing the required libraries
 import streamlit as st
+from pathlib import Path
 
-@st.cache_resource
-def load_format(file_path):
-    with open(file_path) as f:
-        return f.read()
-
-f = load_format('./pages/style.css')
-st.markdown(f'<style>{f}</style>',unsafe_allow_html=True)
-
+ROOT_DIR = Path(__file__).parent.parent
+IMG_DIR = ROOT_DIR / "pages" / "data_exploration_images"
 
 st.title('Data Exploration')
 st.header('Checking for correlations between our four variables')
@@ -17,7 +11,12 @@ st.write('''This plot shows every combination of our different
          We will call these the data **attributes**.''')
 st.write('**Can you pick out which Iris type is easiest to distinguish from the other two?** (Answer question 1)')
 
-st.image(f'./pages/data_exploration_images/plot1.png')
+# Robust Plot 1 Loading
+plot1 = IMG_DIR / "plot1.png"
+if plot1.exists():
+    st.image(str(plot1))
+else:
+    st.warning("Plot 1 image missing.")
 
 st.header('Can we simplify the view of our data?')
 st.write('''Yes! There are methods to reduce dimensionality of data.
@@ -25,6 +24,9 @@ st.write('''Yes! There are methods to reduce dimensionality of data.
          it much easier to view our data! The below plot shows our data in two 
          dimensions (so instead of our four attributes we have two).''')
 
-st.image(f'./pages/data_exploration_images/plot2.png')
+# Robust Plot 2 Loading
+plot2 = IMG_DIR / "plot2.png"
+if plot2.exists():
+    st.image(str(plot2))
 
 st.write('Does this simplified plot still have a single Iris type that is easiest to identify?')
